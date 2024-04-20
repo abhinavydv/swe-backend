5044337f422b78cb6a340bf6a9aa2a9501dc85e0
tejalkul <tejalk00@gmail.com>
2024-04-20 15:12:39 +0530

    changed main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import user, hotels
+from routers import user, hotels,search
 import models as _
 from config.db import engine, Base
 
@@ -24,6 +24,7 @@ app.add_middleware(
 
 app.include_router(user.router)
 app.include_router(hotels.router)
+app.include_router(search.router)
 
 if __name__ == "__main__":
     import uvicorn
